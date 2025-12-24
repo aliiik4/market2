4fae8d5fe6daaab587e935ae2d8e05b3e6914ca1
aliiik4 <94960598+aliiik4@users.noreply.github.com>
2025-12-24 21:47:23 +0330

    Create config.js for API configurations
    This file contains API configuration details for CoinCap, CoinGecko, and Bitquery. It should be added to .gitignore to prevent it from being tracked by Git.

---
+++ config.js
@@ -0,0 +1,19 @@
+<!--
+بخش 1: فایل config.js - این فایل را جداگانه ایجاد و در گیت‌هاب ایگنور کنید
+-->
+
+// فایل: config.js - در .gitignore قرار دهید
+const API_CONFIG = {
+    COINCAPIO: {
+        KEY: 'aac00e522d92a0825858fc9f11d3f6f655d8cf8eff8bc8d515092e0bc7b7d9cc',
+        BASE_URL: 'https://api.coincap.io/v2'
+    },
+    COINGECKO: {
+        KEY: 'CG-WFM5kXKGJNJyKb6T12YaAUPf',
+        BASE_URL: 'https://api.coingecko.com/api/v3'
+    },
+    BITQUERY: {
+        KEY: 'b8b68b31-deb4-4a93-9803-fde35a508370',
+        BASE_URL: 'https://graphql.bitquery.io'
+    }
+};
